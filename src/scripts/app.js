ba34adae1f9f56ebcb57a18e1a4ff4390c5094fd
Nicolas Lefebvre <nicolas.lefebvre@student.heaj.be>
2025-05-22 14:18:47 +0200

    initialisation navigation

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,9 @@
 'use strict'
+const menu = document.querySelector(".header__nav");
+const menuBtn = document.querySelector(".menu__btn");
+const blurOverlay = document.querySelector(".blur-overlay");
+
+menuBtn.addEventListener("click", () => {
+    menu.classList.toggle("menu--open");
+    blurOverlay.classList.toggle("hidden");
+});
