8fc441f477f2639823912fa9eede657a99468545
Nicolas Lefebvre <nicolas.lefebvre@student.heaj.be>
2025-05-27 14:40:46 +0200

    ajout ancrage

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -137,3 +137,15 @@ document.querySelectorAll('.boutique__produit').forEach(produit => {
   }
 });
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
