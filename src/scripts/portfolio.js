4355c172d2888d1b63aad4103e374f497cb1e379
NiicoSnow <nicolas.lefebvre@student.heaj.be>
2025-06-01 23:00:16 +0200

    fin accueil + ajout switch page en 1 html

--- src/scripts/portfolio.js
+++ src/scripts/portfolio.js
@@ -4,3 +4,23 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 import ScrollToPlugin from "gsap/ScrollToPlugin";
 
 gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
+
+document.addEventListener("DOMContentLoaded", () => {
+  const accueil = document.getElementById("accueil");
+  const projets = document.getElementById("projets");
+  const boutonProjets = document.querySelector(".accueil__projets");
+  const boutonRetour = document.querySelector(".projets__retour");
+
+  // Affiche accueil au démarrage
+  accueil.classList.add("accueil--active");
+
+  boutonProjets.addEventListener("click", () => {
+    accueil.classList.remove("accueil--active");
+    projets.classList.add("projets--active");
+  });
+
+  boutonRetour.addEventListener("click", () => {
+    projets.classList.remove("projets--active");
+    accueil.classList.add("accueil--active");
+  });
+});
